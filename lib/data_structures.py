2d22803814782209235a102a10b02a4a206cac36
Victor <victorac2324@gmail.com>
2024-04-24 21:21:44 -0400

    data structures lab finished

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -15,24 +15,53 @@ spicy_foods = [
         "heat_level": 6,
     },
 ]
-
+# defining function get_names takes in the paramater spicy_foods 
 def get_names(spicy_foods):
-    pass
+    
+    return [food['name'] for food in spicy_foods]
+    
+    
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food['heat_level'] > 5]
+    
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        heat_level ="🌶" * food['heat_level']
+        print (f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
+        
+   
+    
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    for food in spicy_foods:
+        if food['cuisine'] == cuisine:
+            return food
     pass
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    print_spicy_foods(get_spiciest_foods(spicy_foods))
+
+    # for food in spicy_foods:
+    #     if food['heat_level'] >= 5:
+    #           print (f"{food['name']} ({food['cuisine']}) | Heat Level: {food["heat_level"]}")
+            # why dis dont wotk 
+                #because it is printing the level of spicyness and not the emoji peper
 
 def get_average_heat_level(spicy_foods):
+    
+    sum=0
+    for food in spicy_foods:
+        heat = food ['heat_level']
+        sum += heat
+    average = sum / len(spicy_foods)
+    return average
+
+
     pass
 
 def create_spicy_food(spicy_foods, spicy_food):
+    spicy_foods.append(spicy_food)
+    return spicy_foods
     pass
